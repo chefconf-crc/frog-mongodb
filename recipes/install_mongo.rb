f4b0ab3581347419b7fe2a4359ca2fa8a426c3fa
emujoyce <emujoyce@gmail.com>
2016-07-11 17:49:23 +0000

    fix error

--- recipes/install_mongo.rb
+++ recipes/install_mongo.rb
@@ -5,11 +5,3 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 package 'mongodb-org'
-
-package 'mongodb-org-server'
-
-package 'mongodb-org-mongos'
-
-package 'mongodb-org-shell'
-
-package 'mongodb-org-tools'
