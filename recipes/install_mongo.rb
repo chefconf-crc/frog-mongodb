5b4fb7d38fd38842849a9fe0e479d19eb10e29fb
emujoyce <emujoyce@gmail.com>
2016-07-11 17:47:20 +0000

    adding recipe

---
+++ recipes/install_mongo.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: .
+# Recipe:: install_mongo
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+package 'mongodb-org'
+
+package 'mongodb-org-server'
+
+package 'mongodb-org-mongos'
+
+package 'mongodb-org-shell'
+
+package 'mongodb-org-tools'
